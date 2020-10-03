83505d87df743a986b61211646495e85f32bf34b
AnkanPaul7 <54813730+AnkanPaul7@users.noreply.github.com>
2020-10-04 03:05:03 +0530

    Create returnNextNumber.cpp

---
+++ 1. Very Easy/returnNextNumber.cpp
@@ -0,0 +1,5 @@
+//Problem-Task : Create a function that takes a number as an argument, add one to the number, and return the result.
+//Problem Link : https://edabit.com/challenge/QgCMMMyMmckvHbfKv
+int addition(int a) {
+	return ++a;
+}
